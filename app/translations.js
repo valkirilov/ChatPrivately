ad6ea56292e5eed25fafd011687bc53657a77981
Valentin Kirilov <val.kirilov@gmail.com>
2014-11-29 21:33:07 +0200

    iframe added.

--- app/translations.js
+++
@@ -1,5 +0,0 @@
-angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
-/* jshint -W100 */
-    gettextCatalog.setStrings('bg', {"Bootstrap starter template":"Добре долши!"});
-/* jshint +W100 */
-}]);
